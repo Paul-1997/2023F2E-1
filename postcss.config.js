45a13e58ddcb3f742feab201af277593481c4ebb
unknown <poola12320@gmail.com>
2023-11-18 19:33:43 +0800

    11/18- 1

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,6 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    require('postcss-import'),
-    require('tailwindcss/nesting')(require('postcss-nesting')),
-    require('tailwindcss'),
-    require('autoprefixer'),
   },
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding : '.75rem'
+      },
+      colors: {
+        // Configure your color palette here
+          primary : '#FFBB18',
+          secondary : '#006C51',
+          third : '#006491',
+          light :'#BEBEBE',
+          dark: '#333333',
+      },
+      fontFamily:{
+        GenSekiGothic : 'GenSekiGothic,sans-serif',
+        Montserrat : "Montserrat, sans-serif;"
+      }
+    },
   },
   plugins: [],
 }
